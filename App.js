57d52a488fe3bfd0f745602854cf41ce5a23ece6
Andrew Acomb <andrewacomb2021@u.northwestern.edu>
2021-01-15 11:44:18 -0600

    Added fetch from Firebase RTDB

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback, useEffect, useState } from "react";
+import { firebase } from "./utils/firebase";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { util } from "prettier";
+
+const database = firebase.database();
 
 export default function App() {
+  const [resumeName, updateResumeNameInternal] = useState("");
+
+  // Use callback as to not constantly recreate function definition
+  const updateResumeName = useCallback(
+    (snap) => {
+      const val = snap.val();
+      return val && updateResumeNameInternal(val);
+    },
+    [updateResumeNameInternal]
+  );
+
+  useEffect(() => {
+    const db = database.ref();
+    db.on("value", updateResumeName, window.alert);
+
+    return () => {
+      db.off("value", updateResumeName);
+    };
+  }, [database, updateResumeName]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>ResumeSync! Make your resume and website with the click of a button</Text>
-      <Text style={styles.text}>You fill in a form and push a button. Amazing!</Text>
+      <Text style={styles.text}>
+        ResumeSync: Make your resume and website with the click of a button
+      </Text>
       <Text style={styles.text}>------------------------------</Text>
-      <Button title="Like this"></Button>
+      <Text style={styles.text}>Name: {resumeName.name}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,12 +42,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#808080',
-    fontSize: 20
-  }
+    color: "#808080",
+    fontSize: 20,
+  },
 });
