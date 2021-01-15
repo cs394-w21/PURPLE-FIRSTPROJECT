13a5a7b5a03343f0348f2b895cb103c14dcf53db
Andrew Acomb <andrewacomb2021@u.northwestern.edu>
2021-01-15 09:13:38 -0600

    Added button on title page

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>ResumeSync! Make your resume and website with the click of a button</Text>
       <Text style={styles.text}>You fill in a form and push a button. Amazing!</Text>
+      <Button title="Like this"></Button>
       <StatusBar style="auto" />
     </View>
   );
