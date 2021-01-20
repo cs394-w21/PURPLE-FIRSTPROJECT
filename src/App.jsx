8dc0e5bf4ed8d2d91192e5f5a52461302aec02e5
wyy1123 <yuyangwu2022@u.northwestern.edu>
2021-01-21 01:12:15 +0800

    Began adding message rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-// import ReactPDF from '@react-pdf/renderer';
+import ReactPDF from "@react-pdf/renderer";
 import useResume from "./hooks/useResume";
 
+console.log(ReactPDF);
 function App() {
   const { loading, resume } = useResume("unique-ID");
   if (loading) return <h1>Loading...</h1>;
   console.log(resume);
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <a href="/">
+        {" "}
+        If your resume did not start downloading automatically, please click
+        here.
+      </a>
     </div>
   );
 }
