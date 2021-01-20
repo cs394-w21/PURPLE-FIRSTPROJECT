960e3ded370e93cb053f564f408e23c1a4920a97
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-01-20 09:00:10 -0600

    Shares code between react-native and react

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import ReactPDF from '@react-pdf/renderer';
+import useResume from "./hooks/useResume";
 
 function App() {
+  const { loading, resume } = useResume("unique-ID");
+  if (loading) return <h1>Loading...</h1>;
+  console.log(resume);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, world!</h1>
     </div>
   );
 }
