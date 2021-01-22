cf0fe06d334f4aa9a77faa8b6d1878028935aaf2
rohithjayaraman <iamrj19@gmail.com>
2021-01-22 17:32:26 -0600

    fighting to get an editable text input

--- expo-components/ResumeForm.jsx
+++ expo-components/ResumeForm.jsx
@@ -1,20 +1,36 @@
 import React from "react";
-import { View, Button } from "react-native";
-import { Formik, Form, useFormikContext } from "formik";
+import { View, Button, TextInput } from "react-native";
+import { Formik, Form, useFormikContext, useField } from "formik";
 import styles from "../expo-utils/styles";
 import { useResumeForm, resumeSchema } from "../expo-utils/resume-form";
 
 const FormShell = (props) => {
   const { resume, children } = props;
+  console.log("in form shell", resume);
   const { submitForm } = useResumeForm("unique-ID");
   return (
     <Formik
       validationSchema={resumeSchema}
-      initialValues={resume}
+      initialValues={{
+        email: resume.email,
+      }}
       onSubmit={submitForm}
     >
       <Form>{children}</Form>
     </Formik>
+  );
+};
+
+const FormInput = (props) => {
+  const { name } = props;
+  const [field, meta, helpers] = useField(name);
+  console.log("field is", field, "helpers are", helpers, "meta is", meta);
+  return (
+    <TextInput
+      value={field.value}
+      onChangeText={(text) => helpers.setValue(text)}
+      onBlur={helpers.setTouched}
+    />
   );
 };
 
@@ -28,6 +44,7 @@ const ResumeForm = (props) => {
   return (
     <View style={styles.container}>
       <FormShell resume={resume}>
+        <FormInput name="email" value={resume.email} />
         <SubmitButton />
       </FormShell>
     </View>
