97bb607def2ade90ea50742963130484943564ba
wyy1123 <yuyangwu2022@u.northwestern.edu>
2021-01-25 10:50:39 +0800

    added basic info

---
+++ src/BasicInfo.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+// import { StyleSheet, Linking } from "react-native";
+import PropTypes from "prop-types";
+
+/* const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    textAlign: "center",
+  },
+  name: {
+    color: "black",
+    fontSize: 40,
+    padding: 5,
+  },
+  email: {
+    color: "blue",
+    textDecorationLine: "underline",
+    fontSize: 20,
+  },
+  text: {
+    color: "black",
+    fontSize: 20,
+  },
+});
+
+const useEmailLink = (email) => {
+  const gotoEmail = React.useCallback(() => {
+    Linking.openURL(`mailto:${email}`);
+  }, [email]);
+  return gotoEmail;
+};
+*/
+
+const BasicInfo = (props) => {
+  const { name, phone, email, location } = props;
+  // const gotoEmail = useEmailLink(email);
+  return (
+    /*   <View style={styles.container}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.text}>Phone: {phone}</Text>
+      <Text style={styles.email} onPress={gotoEmail}>
+        Email: {email}
+      </Text>
+      <Text style={styles.text}>Location: {location}</Text>
+  </View> */
+    <div>
+      <h1>{name}</h1>
+      <p>Phone: {phone}</p>
+      <p>Email: {email}</p>
+      <p>Location: {location}</p>
+    </div>
+  );
+};
+
+export const BasicInfoProps = {
+  name: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+};
+BasicInfo.propTypes = BasicInfoProps;
+
+export default BasicInfo;
--- src/ResumeView.jsx
+++ src/ResumeView.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+// import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
+// import PropTypes from "prop-types";
+import BasicInfo from "./BasicInfo";
+/* import Education, { EducationProps } from "./Education";
+import Experience, { ExperienceProps } from "./Experience";
+import Skills, { SkillProps } from "./Skills"; */
 
-const ResumeView = () => (
-  <div>
-    <p className="red">Hello world</p>
-  </div>
-);
+/* const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "white",
+    alignItems: "center",
+    padding: 20,
+  },
+  subContainer: {
+    alignItems: "center",
+  },
+  sectionContainer: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+  },
+});
+*/
+
+/**
+ * TODO: Figure out how get resume typed properly
+ */
+/* eslint-disable react/prop-types */
+const ResumeView = (props) => {
+  const { resume } = props;
+  return (
+    <div>
+      <BasicInfo
+        name={resume.name}
+        phone={resume.phone}
+        email={resume.email}
+        location={resume.location}
+      />
+      {/*        <View style={styles.sectionContainer}>
+          <Education education={resume.education} />
+          <Experience experience={resume.experience} />
+          <Skills skills={resume.skills} />
+  </View> */}
+    </div>
+  );
+};
+/* eslint-enable react/prop-types */
 
 export default ResumeView;
