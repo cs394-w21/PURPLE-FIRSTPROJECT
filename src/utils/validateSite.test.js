4d6932d734d13fae1c226b6205108528a190b955
wyy1123 <yuyangwu2022@u.northwestern.edu>
2021-01-29 00:01:41 +0800

    Our first test on site url validity

---
+++ src/utils/validateSite.test.js
@@ -0,0 +1,16 @@
+import validateSiteURL from "./validateSite";
+
+describe("Our validation", () => {
+  test("Validates the correct strings", () => {
+    expect(validateSiteURL("asd")).toEqual(true);
+  });
+  test("Detects the invalid strings1", () => {
+    expect(validateSiteURL("****")).toEqual(false);
+  });
+  test("Detects the invalid strings2", () => {
+    expect(validateSiteURL("nervous-line.surge.sh")).toEqual(false);
+  });
+  test("Detects the invalid strings2", () => {
+    expect(validateSiteURL("ABC")).toEqual(true);
+  });
+});
