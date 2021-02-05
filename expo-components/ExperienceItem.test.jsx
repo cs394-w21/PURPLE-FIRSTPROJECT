fb9ac5cf3dd635292076ae15378bd2c1cfa3df4c
Andrew Acomb <andrewacomb2021@u.northwestern.edu>
2021-02-05 10:46:18 -0600

    Add ExperienceItem unit tests

--- expo-components/ExperienceItem.test.jsx
+++ expo-components/ExperienceItem.test.jsx
@@ -1,21 +1,33 @@
 import React from "react";
-// import "react-native-get-random-values";
-import { Text } from "react-native";
-import { render } from "@testing-library/react-native";
+import { render, fireEvent } from "@testing-library/react-native";
 import { Formik } from "formik";
 import ExperienceItem from "./ExperienceItem";
 
 describe("A component", () => {
-  test("Renders correctly", () => {
+  test("Renders all fields properly", () => {
     const { queryByText } = render(
       <Formik>
         <ExperienceItem index={0} remove={() => {}}></ExperienceItem>
       </Formik>
     );
-
     expect(queryByText("Name")).not.toBe(null);
     expect(queryByText("Start")).not.toBe(null);
     expect(queryByText("Stop")).not.toBe(null);
     expect(queryByText("Role")).not.toBe(null);
+    expect(queryByText("Description")).not.toBe(null);
+  });
+
+  test("Remove button exists and works", () => {
+    const fauxRemove = jest.fn();
+    const { queryByText } = render(
+      <Formik>
+        <ExperienceItem index={0} remove={fauxRemove}></ExperienceItem>
+      </Formik>
+    );
+    const removeButton = queryByText("Remove");
+    expect(removeButton).not.toBe(null);
+
+    fireEvent.press(removeButton);
+    expect(fauxRemove).toHaveBeenCalled();
   });
 });
