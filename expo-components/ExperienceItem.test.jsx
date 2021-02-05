7f02279da6c0cc59d2eec9183a04910ffc33da68
Andrew Acomb <andrewacomb2021@u.northwestern.edu>
2021-02-05 10:33:41 -0600

    Added unit test for rendering Experience

--- expo-components/ExperienceItem.test.jsx
+++ expo-components/ExperienceItem.test.jsx
@@ -2,14 +2,20 @@ import React from "react";
 // import "react-native-get-random-values";
 import { Text } from "react-native";
 import { render } from "@testing-library/react-native";
+import { Formik } from "formik";
 import ExperienceItem from "./ExperienceItem";
 
 describe("A component", () => {
   test("Renders correctly", () => {
-    // const { queryByText } = render(<ExperienceItem></ExperienceItem>);
-    //  expect(queryByText("Name")).not.toBe(null);
-    //  expect(queryByText("Start")).not.toBe(null);
-    //  expect(queryByText("Stop")).not.toBe(null);
-    //  expect(queryByText("Role")).not.toBe(null);
+    const { queryByText } = render(
+      <Formik>
+        <ExperienceItem index={0} remove={() => {}}></ExperienceItem>
+      </Formik>
+    );
+
+    expect(queryByText("Name")).not.toBe(null);
+    expect(queryByText("Start")).not.toBe(null);
+    expect(queryByText("Stop")).not.toBe(null);
+    expect(queryByText("Role")).not.toBe(null);
   });
 });
