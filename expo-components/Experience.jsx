18858c7709a10d51c8119b135131e837a202a663
Andrew Acomb <andrewacomb2021@u.northwestern.edu>
2021-02-05 10:23:48 -0600

    Jest working with React Native

--- expo-components/Experience.jsx
+++ expo-components/Experience.jsx
@@ -6,31 +6,32 @@ import PropTypes from "prop-types";
 import FormField from "./FormField";
 import styles from "../expo-utils/styles";
 import FieldList from "./FieldList";
+import ExperienceItem from "./ExperienceItem";
 
-const ExperienceItem = (props) => {
-  const { index, remove } = props;
-  const removeItem = React.useCallback(() => {
-    remove(index);
-  }, [index, remove]);
-  return (
-    <View style={styles.listChunk}>
-      <FormField label="Name" name={`experience[${index}].name`} />
-      <FormField label="Start" name={`experience[${index}].start`} />
-      <FormField label="Stop" name={`experience[${index}].stop`} />
-      <FormField label="Role" name={`experience[${index}].role`} />
-      <FormField
-        label="Description"
-        name={`experience[${index}].description`}
-      />
-      <Button onPress={removeItem} title="Remove" />
-    </View>
-  );
-};
+// const ExperienceItem = (props) => {
+//   const { index, remove } = props;
+//   const removeItem = React.useCallback(() => {
+//     remove(index);
+//   }, [index, remove]);
+//   return (
+//     <View style={styles.listChunk}>
+//       <FormField label="Name" name={`experience[${index}].name`} />
+//       <FormField label="Start" name={`experience[${index}].start`} />
+//       <FormField label="Stop" name={`experience[${index}].stop`} />
+//       <FormField label="Role" name={`experience[${index}].role`} />
+//       <FormField
+//         label="Description"
+//         name={`experience[${index}].description`}
+//       />
+//       <Button onPress={removeItem} title="Remove" />
+//     </View>
+//   );
+// };
 
-ExperienceItem.propTypes = {
-  index: PropTypes.number.isRequired,
-  remove: PropTypes.func.isRequired,
-};
+// ExperienceItem.propTypes = {
+//   index: PropTypes.number.isRequired,
+//   remove: PropTypes.func.isRequired,
+// };
 
 const Experience = () => (
   <FieldList
---
+++ expo-components/ExperienceItem.test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import "react-native-get-random-values";
+import { Text } from "react-native";
+import { render } from "@testing-library/react-native";
+import ExperienceItem from "./ExperienceItem";
+
+describe("A component", () => {
+  test("Renders correctly", () => {
+    // const { queryByText } = render(<ExperienceItem></ExperienceItem>);
+    //  expect(queryByText("Name")).not.toBe(null);
+    //  expect(queryByText("Start")).not.toBe(null);
+    //  expect(queryByText("Stop")).not.toBe(null);
+    //  expect(queryByText("Role")).not.toBe(null);
+  });
+});
